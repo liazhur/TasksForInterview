5a8bbb92aaaa21d73346042e8e6f21b3b5b25b33
Iuliia <Iuliia@192.168.0.10>
2019-04-26 16:53:38 -0300

    Another product solution added

--- TestCanada/Tests/Tests/ArrayProduct.java
+++ TestCanada/Tests/Tests/ArrayProduct.java
@@ -9,7 +9,8 @@ public class ArrayProduct {
 	static int arr[] = { 10, 3, 5, 6, 2 };
 
 	public static void main(String[] args) {
-		Arrays.toString(productArray(arr));
+//		Arrays.toString(productArray(arr));
+		System.out.println(Arrays.toString(productArrayResult(arr)));
 	}
 
 	@Test
@@ -18,8 +19,7 @@ public class ArrayProduct {
 	}
 
 	// Given an array of integers, calculate for each position the product of all
-	// members of the array
-	// except for the integer in current position
+	// members of the array except for the integer in current position
 
 	static int[] productArray(int arr[]) {
 		// Initialize memory to all arrays
@@ -56,4 +56,31 @@ public class ArrayProduct {
 
 		return prod;
 	}
+
+	// solution 2
+	static int[] productArrayResult(int arr[]) {
+		int[] prodArr = new int[arr.length];
+
+		int prod = product(arr);
+
+		// to exclude integer from current position - divide general product to current
+		// element
+		for (int i = 0; i < arr.length; i++) {
+			prodArr[i] = prod / arr[i];
+		}
+		return prodArr;
+	}
+
+	// product of all elements in an array
+	static int product(int arr[]) {
+		int n = arr.length;
+		int product = 1;
+
+		for (int i = 0; i < n; i++) {
+			product *= arr[i];
+		}
+
+		return product;
+	}
+
 }
